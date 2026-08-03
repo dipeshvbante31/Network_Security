cd6b33e8e2c1e44e5a606d7b36e018f236d2b211
dipeshvbante31 <dipeshbante611@gmail.com>
2026-08-03 20:56:51 +0530

    adding setup.py and packages

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages,setup #it will scan through all folders and find __init__.py file and parent folder will become package it self
+from typing import List
+
+def get_requirements()->list[str]:
+
+    """
+    this function willl return list of requirements 
+
+    """
+    requirement_list:list[str]=[]
+    try:
+        with open('requirements.txt','r') as file:
+            #read line from the file 
+
+            lines =file.readlines()
+            #process each line 
+
+            for line in lines:
+                requirement=line.strip()
+                #igonre empty line and -e .
+                if requirement and requirement != '-e .':
+                    requirement_list.append(requirement)
+
+    except FileNotFoundError:
+        print("requirements.txt file not found")
+
+    return requirement_list
+
+setup(
+    name="NetworkSecurity",
+    version="0.0.1",f]
+    author="DipeshVB",
+    author_email="dipeshbante611@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
